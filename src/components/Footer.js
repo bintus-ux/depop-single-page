966f8e996fec44bbf46eabe9ffc7590357284721
Ezerike Caleb Chikere <cezerike@gmail.com>
2024-01-03 11:06:12 +0100

    'update: footer setup'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,124 @@
-import React from 'react'
+import {
+  Wrap,
+  WrapItem,
+  Stack,
+  Flex,
+  Select,
+  useMediaQuery,
+} from '@chakra-ui/react'
+import { FaInstagram } from 'react-icons/fa6'
+import { FaXTwitter } from 'react-icons/fa6'
+import { FaFacebookF } from 'react-icons/fa'
+import { FaTiktok } from 'react-icons/fa'
+import Logo from './Navigation bar/Logo'
 
 const Footer = () => {
-  return <div></div>
+  const currentYear = new Date().getFullYear()
+  function handleScroll() {
+    const pageTop =
+      document.documentElement.scrollTop || document.body.scrollTop
+    const pageBottom = pageTop + window.innerHeight
+    const fadeups = document.querySelectorAll('.fade_up')
+
+    fadeups.forEach((fade_up) => {
+      if (fade_up.getBoundingClientRect().top < pageBottom) {
+        fade_up.classList.add('visible')
+      } else {
+        fade_up.classList.remove('visible')
+      }
+    })
+  }
+
+  document.addEventListener('scroll', handleScroll)
+
+  const [isSmallScreen] = useMediaQuery('(max-width: 767px)')
+
+  const footerItems = [
+    'Blog',
+    'About',
+
+    'Sell on Depop',
+
+    'Depop Amplified',
+
+    'Jobs',
+    'News',
+    'Support',
+    'Terms',
+    'Privacy',
+    'Safety',
+    'Sitemaps',
+    'Cookies',
+  ]
+
+  return (
+    <>
+      {!isSmallScreen && (
+        <Flex
+          as='footer'
+          role='contentinfo'
+          mx='auto'
+          flexWrap='wrap'
+          py={{ base: '12', md: '5' }}
+          px={{ base: '12px', md: '30px' }}
+          style={{ border: '1px solid red' }}>
+          <Stack
+            justify='space-between'
+            direction='row'
+            align='center'
+            gap='100px'>
+            <Wrap py='15px' align='center' width='100%' height='auto'>
+              {footerItems.map((item, i) => (
+                <WrapItem
+                  key={i}
+                  color='rgb(38, 38, 38)'
+                  style={{ textDecoration: 'none' }}
+                  fontSize='15px'
+                  fontFamily='Poppins'
+                  fontWeight='600'
+                  lineHeight='1.5'
+                  textDecoration='none'
+                  paddingRight={5}
+                  cursor='pointer'
+                  _hover={{
+                    fontsize: '20px',
+                  }}>
+                  {item}
+                </WrapItem>
+              ))}
+            </Wrap>
+            <Flex
+              w='600px'
+              justify='space-between'
+              direction='row'
+              align='center'
+              style={{ border: '1px solid red' }}>
+              <FaInstagram />
+              <FaXTwitter />
+              <FaFacebookF />
+              <FaTiktok />
+              <Select
+                w='100px'
+                placeholder='Select option'
+                fontFamily='monospace'>
+                <option value='option1'>Option 1</option>
+                <option value='option2'>Option 2</option>
+                <option value='option3'>Option 3</option>
+              </Select>
+              <Select
+                w='100px'
+                placeholder='Select option'
+                fontFamily='monospace'>
+                <option value='option1'>Option 1</option>
+                <option value='option2'>Option 2</option>
+                <option value='option3'>Option 3</option>
+              </Select>
+            </Flex>
+          </Stack>
+        </Flex>
+      )}
+    </>
+  )
 }
 
 export default Footer
